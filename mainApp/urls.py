360db1586d9a5f865a8b538a883fb550cac72b54
Jeon-Hyeongeun <gusrms611@gmail.com>
2022-04-15 10:27:41 +0900

    메인 설정 페이지 분기

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.showMain),
+    path('calendar', views.showCalendar),
+    path('chart', views.showChart),
+    path('media', views.showMedia),
+    path('remind', views.showRemind),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -3,5 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
+def showMain(request):
     return render(request, 'mainApp/main.html', {})
+
+
+def showCalendar(request):
+    return render(request, 'mainApp/calendar.html', {})
+
+
+def showChart(request):
+    return render(request, 'mainApp/chart.html', {})
+
+
+def showMedia(request):
+    return render(request, 'mainApp/media.html', {})
+
+
+def showRemind(request):
+    return render(request, 'mainApp/remind.html', {})
