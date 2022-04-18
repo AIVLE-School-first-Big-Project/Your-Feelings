049a187c3f82a5abc525dbed80f101d8e1646d1b
Ji Eui Hong <jieuihong97@gmail.com>
2022-04-18 13:57:32 +0900

    minor fix in models.py and settings.py(media root)

--- mainApp/models.py
+++ mainApp/models.py
@@ -9,6 +9,11 @@ class Users(models.Model):
     id = models.CharField(max_length=15, primary_key=True)
     password = models.CharField(max_length=50)
 
+# 감정
+class Emotion(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+
 # 사용자가 작성한 일기들
 class Diary(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,19 +22,14 @@ class Diary(models.Model):
     content = models.CharField(max_length=400)
     open_status = models.IntegerField()
     date = models.DateTimeField()
-    emotion = models.CharField(max_length=50)
-    image = models.ImageField(blank=True, upload_to='images')   # 사용지가 글 쓸 때 첨부하는 사진
+    emotion = models.ForeignKey(Emotion, on_delete=models.PROTECT)
+    image = models.ImageField(blank=True, upload_to='media/')   # 사용지가 글 쓸 때 첨부하는 사진
     
-# 감정
-class Emotion(models.Model):
-    id = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=50)
-
 
 # 감정별 랜덤으로 지정해주는 사진 데베
 class EmotionPictures(models.Model):
     id = models.AutoField(primary_key=True)
-    emotion_id = models.ForeignKey(Emotion, on_delete=CASCADE)
+    emotion_id = models.ForeignKey(Emotion, on_delete=models.CASCADE)
     image = models.ImageField(null=False)
 
 
@@ -39,7 +39,7 @@ class Music(models.Model):
     artist = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
     lyrics = models.CharField(max_length=2000)
-    emotion = models.ForeignKey(Emotion, on_delete=CASCADE)
+    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
 
 class Books(models.Model):
     id = models.AutoField(primary_key=True)
@@ -47,7 +47,7 @@ class Books(models.Model):
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
     summary = models.CharField(max_length=50)
-    emotion = models.ForeignKey(Emotion, on_delete=CASCADE)
+    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
 
 
 class Movies(models.Model):
@@ -58,12 +58,12 @@ class Movies(models.Model):
     actors = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     release_year = models.CharField(max_length=50)
-    emotion = models.ForeignKey(Emotion, on_delete=CASCADE)
+    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
 
 # 감정 기반 추천 (일기 별로 하나)
 class RecommendList(models.Model):
     id = models.AutoField(primary_key=True)
-    post_id = models.ForeignKey(Diary, on_delete=CASCADE)
+    post_id = models.ForeignKey(Diary, on_delete=models.CASCADE)
     rec_movie = models.ForeignKey(Movies, on_delete=models.PROTECT)
     rec_music = models.ForeignKey(Music, on_delete=models.PROTECT)
     rec_book = models.ForeignKey(Books, on_delete=models.PROTECT)
