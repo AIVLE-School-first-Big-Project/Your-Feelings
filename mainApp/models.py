70c3cc13e21c58558df0cdca0ceded74de608a55
cmc9595 <cmc9595@nate.com>
2022-04-18 10:20:20 +0900

    make Class Diary

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Diary(models.Model):
+    user_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=400)
+    open_status = models.IntegerField()
+    date = models.DateTimeField()
+    emotion = models.CharField(max_length=50)
