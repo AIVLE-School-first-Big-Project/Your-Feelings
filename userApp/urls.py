8a3bd5eb71c364344f6bc3e8f548bb052fdd3e63
Ji Eui Hong <jieuihong97@gmail.com>
2022-04-22 14:16:02 +0900

    mypage 생성

--- userApp/urls.py
+++ userApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name='userApp/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path("mypage/", views.mypage, name='mypage')
 ]
