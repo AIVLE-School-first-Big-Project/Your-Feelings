f640dd82f03ecfc9939f31215b240373657a4ba5
jhg2957 <gusrms611@gmail.com>
2022-04-22 11:26:06 +0900

    main page   login, logout 기본 셋팅 완료

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT_URL = '.'
 
+# 로그인 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/main/'
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = '/user/login'
 
 AUTH_USER_MODEL = "userApp.Users"
--- userApp/urls.py
+++ userApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout"),
+    path('login/', auth_views.LoginView.as_view(template_name='userApp/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
