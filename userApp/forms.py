c95116c9f86900b57709efb9fcf36fcebe6251ac
Ji Eui Hong <jieuihong97@gmail.com>
2022-04-29 15:25:41 +0900

    회원가입 드디어

--- userApp/forms.py
+++ userApp/forms.py
@@ -1,14 +1,21 @@
 from django import forms
-from django.contrib import auth
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, PasswordResetForm
-from django.contrib.auth import password_validation, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm
 from .models import Users
-from django.core.exceptions import ValidationError
 
-class SignupForm(forms.ModelForm):
+class SignupForm(UserCreationForm):
+    full_name = forms.CharField(max_length=50, required=True, help_text="이름을 입력해주세요")
+    email = forms.EmailField(max_length=100, required=True, help_text="유효한 이메일을 입력해주세요")
+
     class Meta:
         model = Users
-        fields = ['full_name', 'username', 'password1']
+        fields = ['full_name', 'username', 'password1', 'password2', 'email']
+
+        labels = {
+            'username': '아이디',
+            'password1': '비밀번호',
+            'email': '이메일',
+            'full_name': '이름',
+        }
 
     
     # def clean_username(self):
--- userApp/views.py
+++ userApp/views.py
@@ -6,47 +6,21 @@ from userApp.forms import SignupForm
 from .models import Users
 from django.contrib import auth
 
-# def if_session(request):
-#     username = request.user
-
-#     if username:
-#         user = Users.objects.get(username=username)
-#         return 
-
 
 def signup(request):
-    # if request.method == "POST":
-    #     pw1 = request.POST.get("password")
-    #     pw2 = request.POST.get("password2")
-
-    #     if pw1 == pw2:
-    #         user = Users.objects.create_user(
-    #             username = request.POST.get("username"),
-    #             password = request.POST.get("password"),
-    #             email = request.POST.get("email"),
-    #             full_name = request.POST.get("full_name")
-    #         )
-    #         # auth.login(request, user)
-    #         return redirect("main")
-    #     else:
-    #         return render(request, "userApp/signup.html", {'error': "비밀번호가 일치하지 않습니다"})
-    # return render(request, "userApp/signup.html")
-
-    if request.method == "POST":
+    if request.method == 'POST':
         form = SignupForm(request.POST)
-        context ={}
-        password2 = request.POST.get('password2')
-
         if form.is_valid():
-            if form.password1 != password2:
-                context['error'] = "비밀번호가 일치하지 않습니다."
-                return render(request, "userApp/signup", context)
-            else:
-                form.save()
-                return redirect('user:login')
-        else:
-            form = SignupForm()
-    return render(request, 'userApp/signup')
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=raw_password)
+            auth.login(request, user)
+            return redirect("main")
+    else:
+        form = SignupForm()
+    return render(request, 'userApp/signup.html', {'form': form})
             
 
 def login(request):
@@ -64,7 +38,7 @@ def login(request):
                 return render(request, "userApp/login.html", context)
         except Users.DoesNotExist:
             context['error'] = "존재하지 않는 아이디입니다"
-            return render(request, "userApp/login")
+            return render(request, "userApp/login.html")
         else:
             request.session['username'] = user.username
             request.session['password'] = user.password
