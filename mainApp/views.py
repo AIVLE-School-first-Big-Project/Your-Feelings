58fd37b1011850fa3b485465c93508afe5bf83fa
baekgom78 <dragon70803@gmail.com>
2022-05-06 13:29:41 +0900

    임시 날짜설정

--- mainApp/views.py
+++ mainApp/views.py
@@ -235,8 +235,9 @@ def postDiary(request):
         title=request.POST['title']
         content=request.POST['content']
         public=request.POST['public']
+        date = request.POST['date']
+        #date=datetime.datetime.now().strftime('%Y-%m-%d')
         
-        date=datetime.datetime.now().strftime('%Y-%m-%d')
         api_result=requests.get(KOBERT_API_URL+content).text
 
         description = json.loads(api_result)
